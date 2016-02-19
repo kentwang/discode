741f3d5ad991358f101a9eeac153b368e9d018da
Ketong Wang <kentwang16@gmail.com>
2016-02-18 22:36:09 -0600

    Testing acceptance ratio for paired and unpaired cases
    - add feature generation with augmentation
    - add testing for unpaired case

--- dIBP/dIBP/random_init.m
+++ dIBP/dIBP/random_init.m
@@ -1,3 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Testing sampling algorithm using random feature matrices %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 rng(20160209);
 
 I = 10;
@@ -6,26 +10,55 @@ K = 4;
 L = 6;
 M = max(K, L);
 
+% sample the feature generating probabilities with augmentation
 a = 2; b = 5; c = 1;
 [lambda_u, lambda_v] = bibetarnd(M, a, b, 1);
-mu_u = cumprod(lambda_u);
-mu_v = cumprod(lambda_v);
-mu = [mu_u, mu_v];
+mu_u_aug = cumprod(lambda_u);
+mu_v_aug = cumprod(lambda_v);
+mu = [mu_u_aug, mu_v_aug];
+mu_u = mu_u_aug(1:K);
+mu_v = mu_v_aug(1:L);
 
-mu_u_test = mu_u + normrnd(0, 0.001, M, 1);
-mu_v_test = mu_v + normrnd(0, 0.001, M, 1);
-mu_test = [mu_u_test, mu_v_test];
+mu_u_test_aug = mu_u_aug + normrnd(0, 0.001, M, 1);
+mu_v_test_aug = mu_v_aug + normrnd(0, 0.001, M, 1);
+mu_test = [mu_u_test_aug, mu_v_test_aug];
+mu_u_test = mu_u_test_aug(1:K);
+mu_v_test = mu_v_test_aug(1:L);
+
 
 U = matbinornd(mu_u, I, K);
 V = matbinornd(mu_v, J, L);
 
-UV_mupdf(U, V, mu_u(1:K), mu_v(1:L)) / ...
-    UV_mupdf(U, V, mu_u_test(1:K), mu_v_test(1:L))
+%-- Test computation of acceptance ratio for paired case
+r = 2;
+mur = mu(r, :);
+murp = mu(r + 1, :);
+murm = mu(r - 1, :);
+mur_test = mu_test(r, :);
+murp_test = mu_test(r + 1, :);
+murm_test = mu_test(r - 1, :);
 
-%-- Test computation of acceptance ratio
-UV_mupdf(U, V, mu_u_test(1:K), mu_v_test(1:L)) / ...
-    UV_mupdf(U, V, mu_u(1:K), mu_v(1:L)) * ...
-    mucondpdf(mur, murp, murm, a, b, c)
+UV_mupdf(U(:, r), V(:, r), mu_u_test(r), mu_v_test(r)) / ...
+    UV_mupdf(U(:, r), V(:, r), mu_u(r), mu_v(r)) * ...
+    mucondpdf(mur_test, murp_test, murm_test, a, b, c) / ...
+    mucondpdf(mur, murp, murm, a, b, c) * ...
+    mucondproppdf(mur, murp, murm, a, b, c, K, L) / ...
+    mucondproppdf(mur_test, murp_test, murm_test, a, b, c, K, L)
 
+%-- Test computation of acceptance ratio for unpaired case
+%-- U probability is nullified
+mur(1) = -1; 
+murp(1) = -1; 
+murm(1) = -1;
+mur_test(1) = -1; 
+murp_test(1) = -1; 
+murm_test(1) = -1;
+    
+UV_mupdf(U(:, r), V(:, r), mu_u_test(r), mu_v_test(r)) / ...
+    UV_mupdf(U(:, r), V(:, r), mu_u(r), mu_v(r)) * ...
+    mucondpdf(mur_test, murp_test, murm_test, a, b, c) / ...
+    mucondpdf(mur, murp, murm, a, b, c) * ...
+    mucondproppdf(mur, murp, murm, a, b, c, K, L) / ...
+    mucondproppdf(mur_test, murp_test, murm_test, a, b, c, K, L)
 
 

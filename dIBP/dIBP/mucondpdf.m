e18dbc0663b45df99a374fde733de6dea1664bcc
Ketong Wang <kentwang16@gmail.com>
2016-02-18 22:34:57 -0600

    Unpaired sampling
    Add modifications for unpaired (marginal) probability sampling

--- dIBP/dIBP/mucondpdf.m
+++ dIBP/dIBP/mucondpdf.m
@@ -1,7 +1,8 @@
 function output = mucondpdf(mur, murp, murm, a, b, c)
 % Conditional probability Prob(mur | murp, murm) in Eq. (2.20)
 % Referred to Eq. (19) in Xuan et. al [2015]
-% Note: 1) all mu's are TWO-DIM!
+% Note: 1) mu's are TWO-DIM when paired and ONE-DIM when not!
+%       2) mu's dimensions have to be identical
 %       2) only need bi-variate pdf
 
 % Arguments:
@@ -9,10 +10,30 @@ function output = mucondpdf(mur, murp, murm, a, b, c)
 % murp -- mu_{r+1}
 % murm -- mu_{r-1}
 % a, b, c -- parameters of bi-variate beta distribution
+
+% Todo:
+%   - verify the consistency of -1
     
-    difmum = mur ./ murm;
-    difmup = murp ./ mur;
-    output = bibetapdf(difmum(1), difmum(2), a, b, c) * ...
-        bibetapdf(difmup(1), difmup(2), a, b, c) / ...
-        prod(murm) / prod(mur);
+    % check if mu contains -1 that indicates non-pairing
+    % if yes, the dimension should be all 1's for mur, murp, murm
+    if ~any(mur == -1)
+        % both muru and murv exist and bibeta(a, b, c) is used
+        difmum = mur ./ murm;
+        difmup = murp ./ mur;
+        output = bibetapdf(difmum(1), difmum(2), a, b, c) * ...
+            bibetapdf(difmup(1), difmup(2), a, b, c) / ...
+            prod(murm) / prod(mur);
+    elseif find(mur == -1) == 1
+        % muru is null and beta(b, c) is used
+        difmum = mur(2)/ murm(2);
+        difmup = murp(2)/ mur(2);
+        output = betapdf(difmum, b, c) * betapdf(difmup, b, c) / ... 
+            murm(2) / mur(2);
+    elseif find(mur == -1) == 2
+        % murv is null and beta(a, c) is used
+        difmum = mur(1)/ murm(1);
+        difmup = murp(1)/ mur(1);
+        output = betapdf(difmum, a, c) * betapdf(difmup, a, c) / ... 
+            murm(1) / mur(1);
+    end
 end
--- dIBP/dIBP/mucondproppdf.m
+++ dIBP/dIBP/mucondproppdf.m
@@ -1,7 +1,19 @@
-function output = mucondproppdf(mur, murp, murm, a, b, c, K)
+function output = mucondproppdf(mur, murp, murm, a, b, c, K_plus, L_plus)
 % Proposal pdf with truncated beta's in Eq (2.21).
 % Note: check the equations
-    
-    output = truncbetapdf(mur(1), a/K, c, murp(1), murm(1)) * ...
-        truncbetapdf(mur(2), b/K, c, murp(2), murm(2));
+
+% Todo:
+%   - verify the consistency of -1
+
+    if ~any(mur == -1)
+        % both muru and murv exist and two truncated beta are used
+        output = truncbetapdf(mur(1), a/K_plus, c, murp(1), murm(1)) * ...
+            truncbetapdf(mur(2), b/L_plus, c, murp(2), murm(2));
+    elseif find(mur == -1) == 1
+        % muru is null and truncbeta(b/L, c) is used
+        output = truncbetapdf(mur(2), b/L_plus, c, murp(2), murm(2));
+    elseif find(mur == -1) == 2
+        % murv is null and truncbeta(a/K, c) is used
+         output = truncbetapdf(mur(1), a/K_plus, c, murp(1), murm(1));
+    end
 end

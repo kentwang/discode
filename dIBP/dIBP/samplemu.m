b2c298629e223038806f42ca4badeab5b7d90a19
Ketong Wang <kentwang16@gmail.com>
2016-02-20 23:40:52 -0600

    Append zeros for U/V and mu's (testing unfinished)
    - adjust truncation bounds for starting and trailing mu's
    - maybe too wide (0/1). But looked fine.

--- dIBP/dIBP/samplemu.m
+++ dIBP/dIBP/samplemu.m
@@ -1,19 +1,58 @@
-function output = samplemu(U, V, mu_u, mu_v)
+function output = samplemu(U, V, mu_u, mu_v, a, b)
 % Sample feature generating probabilities using MH algorithm
 
 % INPUT
-%   - U, V, mu_u, mu_v: current feature matrices and probabilityes
-
-% NOTE
-%   - Dimensions of U (V) and mu_u (mu_v) have to be matched
+%   - U, V, mu_u, mu_v: current real feature matrices and probabilityes
+%   - a, b: controling parameters of dIBP
     
 % TODO
 %   - Warning unmatched dimensions
+%   - recheck 0 and 1 as the bounds. Maybe a noise +/- is OK
 
     K = length(mu_u);
     L = length(mu_v);
-    Mm = min(K, L);
-    Mp = max(K, L);
+    M = max(K, L);
     
+    % append zerors
+    if(K < M) 
+        U = [U, zeros(I, M - K)];
+        mu_u = [mu_u; zeros(M - K, 1)];
+    else
+        V = [V, zeros(J, M - J)];
+        mu_v = [mu_v; zeros(M - L, 1)];
+    end
     
+    % MH sampling
+    for r = 1:M
+        % input of each sampling        
+        if r == 1 % propose for the first mu
+            lower_u = mu_u(r + 1);
+            lower_v = mu_v(r + 1);
+            upper_u = 1;
+            upper_v = 1;
+        elseif r == M % propose for the last mu
+            lower_u = 0;
+            lower_v = 0;
+            upper_u = mu_u(r - 1);
+            upper_v = mu_v(r - 1);
+        else % update others
+            lower_u = mu_u(r + 1);
+            lower_v = mu_v(r + 1);
+            upper_u = mu_u(r - 1);
+            upper_v = mu_v(r - 1);
+        end
+        
+        mu_u_r_prop = truncbetarnd(a / K, 1, lower_u, upper_u);
+        mu_v_r_prop = truncbetarnd(b / L, 1, lower_v, upper_v);
+        
+        accept_r = min(1, UV_mupdf(U(:, r), V(:, r), mu_u_r_prop, ...
+            mu_v_r_prop) / UV_mupdf(U(:, r), V(:, r), mu_u(r), ...
+            mu_v(r)) * mucondpdf(mur_test, murp_test, murm_test, ...
+            a, b, c) / mucondpdf(mur, murp, murm, a, b, c) * ...
+            mucondproppdf(mur, murp, murm, a, b, c, K, L) / ...
+            mucondproppdf(mur_test, murp_test, murm_test, a, b, c, K, L));
+    end
 end
+
+
+

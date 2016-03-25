4706c8d9ac5ec503b2e53c8c6de419b5967d9ca5
Ketong Wang <kentwang16@gmail.com>
2016-03-25 14:24:22 -0500

    Z is n needed for related sampling

--- dIBP/dIBP/W_expand.m
+++ dIBP/dIBP/W_expand.m
@@ -1,4 +1,4 @@
-function result = W_expand(new_k, dim, U, V, W, nuep, sigma_w)
+function result = W_expand(new_k, dim, Z, U, V, W, nuep, sigma_w)
 % expand rows or columns of W according to the generation of
 % new features of U or V. Individual element of the new vector of
 % W is sampled.
@@ -17,10 +17,10 @@ function result = W_expand(new_k, dim, U, V, W, nuep, sigma_w)
 		% Gibbs  and MH sampling of new elements
 		for d = 1:new_k
 			for l = 1:L
-				l_curr = log(normpdf(W(K + d, l), 0, sigma_w)) + Z_UVWlogpdf(U, V, W, nuep);
+				l_curr = log(normpdf(W(K + d, l), 0, sigma_w)) + Z_UVWlogpdf(Z, U, V, W, nuep);
 				w_prop = normrnd(W(K + d, l), sigma_w); % simply use normal proposal
 				W(K + d, l) = w_prop;
-				l_prop = log(normpdf(W(K + d, l), 0, sigma_w)) + Z_UVWlogpdf(U, V, W, nuep);
+				l_prop = log(normpdf(W(K + d, l), 0, sigma_w)) + Z_UVWlogpdf(Z, U, V, W, nuep);
 				acc_w = exp(min(0, l_prop - l_curr));
 
 				% printf('iter d = %d, l = %d; l_curr = %f, l_prop = %f; w_prop = %f; accept ratio = %f \n', d, l, l_curr, l_prop, w_prop, acc_w);
@@ -34,10 +34,10 @@ function result = W_expand(new_k, dim, U, V, W, nuep, sigma_w)
 		W(:, (L + 1):(L + new_k)) = 0;
 		for d = 1:new_k
 			for k = 1:K
-				l_curr = normpdf(W(k, L + d), 0, sigma_w) * exp(Z_UVWlogpdf(U, V, W, nuep));
+				l_curr = normpdf(W(k, L + d), 0, sigma_w) * exp(Z_UVWlogpdf(Z, U, V, W, nuep));
 				w_prop = normrnd(W(k, L + d), sigma_w);
 				W(k, L + 1) = w_prop;
-				l_prop = normpdf(W(k, L + d), 0, sigma_w) * exp(Z_UVWlogpdf(U, V, W, nuep));
+				l_prop = normpdf(W(k, L + d), 0, sigma_w) * exp(Z_UVWlogpdf(Z, U, V, W, nuep));
 				acc_w = exp(min(0, l_prop - l_curr));
 
 				if rand < acc_w

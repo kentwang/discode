84780fde84b42a8fc8337e9f9634625eb8032783
Ketong Wang <kentwang16@gmail.com>
2016-03-24 20:38:38 -0500

    Posterior pdf of stacked W

---
+++ dIBP/dIBP/tW_tZFpdf.m
@@ -0,0 +1,14 @@
+function result = tW_tZFpdf(W, Z, U, V, nuep, sigma_w)
+% pdf of posterior of stacked W
+
+	[I, J] = size(Z);
+	K = size(U, 2); L = size(V, 2);
+	F = kron(V, U);
+	tZ = Z(:);
+
+
+	MU = inv(F' * F + eye(K * L, K * L) / sigma_w^2) * F' * (tZ - nuep * ones(I * J, 1));
+	SIGMA = inv(F' * F + eye(K * L, K * L) / sigma_w^2);
+
+	result = mvnpdf(W(:)', MU', SIGMA);
+end

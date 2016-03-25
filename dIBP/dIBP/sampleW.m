7d5ce5749bfb1228b1bd6cd6c8e69d5b96ac8fa4
Ketong Wang <kentwang16@gmail.com>
2016-03-24 20:36:25 -0500

    Sample W
    - sample W from its stacked version of posterior distribution

---
+++ dIBP/dIBP/sampleW.m
@@ -0,0 +1,18 @@
+function result = sampleW(Z, U, V, nuep, sigma_w)
+% sample W from its stacked posterior distribution
+
+% TEST
+% U = U_true; V = V_true; Z = Z_true;
+
+	[I, J] = size(Z);
+	K = size(U, 2); L = size(V, 2);
+	F = kron(V, U);
+	tZ = Z(:);
+
+
+	MU = inv(F' * F + eye(K * L, K * L) / sigma_w^2) * F' * (tZ - nuep * ones(I * J, 1));
+	SIGMA = inv(F' * F + eye(K * L, K * L) / sigma_w^2);
+	tW_new = mvnrnd(MU, SIGMA)';
+
+	result = reshape(tW_new, K, L);
+end

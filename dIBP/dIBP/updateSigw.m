541de82e412252f17669fb97826da2ec87139f86
Ketong Wang <kentwang16@gmail.com>
2016-03-24 20:38:57 -0500

    Update sigma_w

---
+++ dIBP/dIBP/updateSigw.m
@@ -0,0 +1,18 @@
+function result = updateSigw(sigma_w, U, V, W, Z, nuep)
+% update sigma_w using MH sampling
+	
+	% propose sigma_w using uniform perturbation
+	if rand < .5
+		sigma_w_prop = sigma_w + rand/20;
+	else
+		sigma_w_prop = sigma_w - rand/20;
+	end
+
+	sigma_w_acc = min(1, tW_tZFpdf(W, Z, U, V, nuep, sigma_w_prop) / tW_tZFpdf(W, Z, U, V, nuep, sigma_w));
+
+	if rand < sigma_w_acc
+		result = sigma_w_prop;
+	else
+		result = sigma_w;
+	end
+end

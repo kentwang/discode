734bd4a76f67d66bfff89b47cf31826bb9a81bc1
Ketong Wang <kentwang16@gmail.com>
2016-03-24 20:33:14 -0500

    Joint pdf of MU
    - derivation is again from the NFM dIBP paper

--- dIBP/dIBP/mujoinpdf.m
+++ dIBP/dIBP/mujoinpdf.m
@@ -1,18 +1,21 @@
-function output = mujoinpdf(mu, a, b, c)
-% Joint pdf of mu_u and mu_v. Eq. (19) second equality sign
-% Output: return a density vector of length of mu
+function result = mujoinpdf(mu, a, b)
+% Joint pdf of a pair of vector mu_u and mu_v. Return a real-valued density
+% This function is used for sampling from posterior of a and b
 
-% Note: THIS FUNCTION IS NOT NECESSARY!
+% Needs to be checked and tested
 
     n = size(mu, 1);
     if n == 1
-        output = bibetapdf(mu(1), mu(2), a, b, c);
+        result = bibetapdf(mu(1), mu(2), a, b, 1)
     else
-        output = zeros(n, 1);
-        output(1) = bibetapdf(mu(1, 1), mu(1, 2), a, b, c);
+        result = zeros(n, 1);
+        result(1) = bibetapdf(mu(1, 1), mu(1, 2), a, b, 1);
         for i=2:n
             difmu = mu(i, :) ./ mu(i-1, :);
-            output(i) = bibetapdf(difmu(1), difmu(2), a, b, c) / prod(mu(i-1, :));
+            result(i) = bibetapdf(difmu(1), difmu(2), a, b, 1) / prod(mu(i-1, :));
+            % printf('value of p_i: %f \n', result(i));
+            % printf('values of diff diff mu_i: [%f, %f] \n', difmu(1), difmu(2));
         end
+        result = prod(result);
     end
 end

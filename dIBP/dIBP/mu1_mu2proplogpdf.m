819a4d011e9c9ff14d634e4a4cb1201396e7a965
Ketong Wang <kentwang16@gmail.com>
2016-04-06 08:28:17 -0500

    Proposed log pdf of mu_1 | mu_2

---
+++ dIBP/dIBP/mu1_mu2proplogpdf.m
@@ -0,0 +1,8 @@
+function result = mu1_mu2proplogpdf(mu_1, mu_2, a, b)
+% proposed conditional pdf of the first pair of mu
+
+% NOTE
+%	- a and b should be a/K and b/L in real sampling. See Xuan[2015]
+
+	result = log(truncbetapdf(mu_1(1), a, b, mu_2(1), 1)) + log(truncbetapdf(mu_1(2), a, b, mu_2(2), 1));
+end

6302d5e29bf2b35d2d034ce43bb29bc8994d9263
Ketong Wang <kentwang16@gmail.com>
2016-04-10 17:51:49 -0500

    Use log version

--- dIBP/dIBP/mujoinpdf.m
+++ dIBP/dIBP/mujoinpdf.m
@@ -6,13 +6,13 @@ function result = mujoinpdf(mu, a, b)
 
     n = size(mu, 1);
     if n == 1
-        result = bibetapdf(mu(1), mu(2), a, b, 1)
+        result = exp(bibetalogpdf(mu(1), mu(2), a, b, 1));
     else
         result = zeros(n, 1);
-        result(1) = bibetapdf(mu(1, 1), mu(1, 2), a, b, 1);
+        result(1) = exp(bibetalogpdf(mu(1, 1), mu(1, 2), a, b, 1));
         for i=2:n
             difmu = mu(i, :) ./ mu(i-1, :);
-            result(i) = bibetapdf(difmu(1), difmu(2), a, b, 1) / prod(mu(i-1, :));
+            result(i) = exp(bibetalogpdf(difmu(1), difmu(2), a, b, 1)) / prod(mu(i-1, :));
             % printf('value of p_i: %f \n', result(i));
             % printf('values of diff diff mu_i: [%f, %f] \n', difmu(1), difmu(2));
         end

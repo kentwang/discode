1f16d0d7434ff5658eab9d34866be932ac432d4a
Ketong Wang <kentwang16@gmail.com>
2016-04-15 11:45:47 -0500

    sample nu_epsilon with W integrated W

---
+++ dIBP/dIBP/sampleNuep2.m
@@ -0,0 +1,10 @@
+function result = sampleNuep2(Z, U, V, K_plus, L_plus, sigma_w)
+% sample nuep from its posterior distribution with W integrated out
+	[I, J] = size(Z);
+	tz = Z(:);
+	F = kron(V(:, 1:L_plus), U(:, 1:K_plus));
+
+	mu = tz'*(eye(I*J) - F*(F'*F + eye(K_plus*L_plus)/sigma_w^2)^-1*F')*ones(I*J, 1)/(I*J + 1);
+	sigma = 1/(I*J + 1);
+	result = normrnd(mu, sigma);
+end
